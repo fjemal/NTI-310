d398ee6fa5eebe0a6ff94bc3418908beceadea7f
fjemal <fjemal@users.noreply.github.com>
2017-03-19 00:17:14 -0700

    Update postgres.sql

--- config_scripts/postgres.sql
+++ config_scripts/postgres.sql
@@ -1,6 +1,6 @@
 ALTER USER postgres WITH PASSWORD 'P@ssw0rd1';
 CREATE DATABASE project1;
-CREATE USER project1 WITH PASSWORD 'P@ssw0rd1';
+CREATE USER myproject1 WITH PASSWORD 'P@ssw0rd1';
 ALTER ROLE project1 SET client_encoding TO 'utf8';
 ALTER ROLE project1 SET default_transaction_isolation TO 'read committed';
 ALTER ROLE project1 SET timezone TO 'UTC';
